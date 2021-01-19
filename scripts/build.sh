ce7fde6c39bb47737ccac190c24fa5be64ca6b80
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-19 20:26:36 +0100

    feat-#57: update install and build sh scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,8 @@ cd microservices/otasoft-mail && yarn build && cd -
 cd microservices/otasoft-booking && yarn build && cd -
 cd microservices/otasoft-customer && yarn build && cd -
 cd microservices/otasoft-catalog && yarn build && cd -
+cd microservices/otasoft-payment && yarn build && cd -
+cd ml/otasoft-ml && yarn build && cd -
+cd ml/otasoft-ml/client && yarn build && cd -
+cd frontends/vue-otasoft && yarn build && cd -
+cd frontends/react-otasoft && yarn build && cd -
--- scripts/install.sh
+++ scripts/install.sh
@@ -6,3 +6,8 @@ cd microservices/otasoft-mail && yarn && cd -
 cd microservices/otasoft-booking && yarn && cd -
 cd microservices/otasoft-customer && yarn && cd -
 cd microservices/otasoft-catalog && yarn && cd -
+cd microservices/otasoft-payment && yarn && cd -
+cd ml/otasoft-ml && yarn && cd -
+cd ml/otasoft-ml/client && yarn && cd -
+cd frontends/vue-otasoft && yarn && cd -
+cd frontends/react-otasoft && yarn && cd -
